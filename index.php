95ffa8b82d6898a38aea65cc046313bfe4216d4b
Yuta Tsubakimoto <dev.tsubaki@gmail.com>
2013-04-29 21:57:04 +0900

    create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once('config.php');
+require_once('codebird.php');
+
+?>
+<!doctype html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<title>Home : Twitter connect php 2</title>
+</head>
+<body>
+	<h1>ホーム画面</h1>
+	<p>tsubakimoto_sのTwitterアカウントでログインしています。</p>
+	<p><a href="logout.php">[ログアウト]</a></p>
+</body>
+</html>
